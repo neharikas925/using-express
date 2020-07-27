c127825813a9cd29d27dc3aa453f9ac07ed46a0b
neharikas925 <neharikas926@gmail.com>
2020-07-28 02:31:03 +0530

    mess

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-const express=require('express');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
-//express app
-const app=express();
+// express app
+const app = express();
 
-//register view engine
-app.set('view engine','ejs');
+// connect to mongodb & listen for requests
+const dbURI = 'mongodb+srv://netninja:pass123@nodetuts.pzv7k.mongodb.net/node-tuts?retryWrites=true&w=majority';
 
-//listen for requests
-app.listen(5000);
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
-app.get('/',(req,res)=>{
-    //res.send('<p>home page</p>');
-    res.render('index');
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
-app.get('/about',(req,res)=>{
-    
-    res.send('<h1>give me</h1>');
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 
-//redirects
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
-//redirects
-app.use((req,res)=>{
-    
-    res.status(404).render('404');
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
