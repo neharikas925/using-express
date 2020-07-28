9dc70931dd8d2187977720cd8300050e456b5fc5
neharikas925 <neharikas926@gmail.com>
2020-07-28 17:22:20 +0530

    mess

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const dbURI = 'mongodb+srv://netninja:pass123@nodetuts.pzv7k.mongodb.net/node-tuts?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => app.listen(5000))
   .catch(err => console.log(err));
 
 // register view engine
